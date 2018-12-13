4d05c89be3f2727a3fe238a59fa2513662136632
Erikcaldwell <39847875+Erikcaldwell@users.noreply.github.com>
2018-12-12 20:16:47 -0800

    Add files via upload

--- handles.py
+++ handles.py
@@ -13,7 +13,7 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 def get_handles():
     # Grabbing twitter handles to pass to analyzer
-    me = 'erikcaldwell'
+    me = 'tjg_developer'
 
     tweets = api.search(f'@{me} Analyze:')['statuses']
 
